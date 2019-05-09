259a589d7a2ef8dd2ceb5cc6c5d268e34ab735be
william-parker <edwinious@gmail.com>
2019-05-09 18:36:59 -0400

    got task names to appear in browser

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import Todo from './components/TodoComponents/Todo';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        task: 'Organize Garage',
+        completed: false
+      },
+      {
+        id: 2,
+        task: 'Bake Cookies',
+        completed: false
+      },
+      {
+        id: 3,
+        task: 'Practice Saxophone',
+        completed: false
+      }
+    ]
+  }
+
   render() {
+    // console.log(this.state.todos)
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <Todo todos={this.state.todos}/>
       </div>
     );
   }
